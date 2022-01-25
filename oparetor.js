7458885d59a63107da501f561e9f062a76e5d8f1
khaled39ab <khaled39ab@gmail.com>
2022-01-25 19:25:47 +0600

    string-methods

---
+++ oparetor.js
@@ -0,0 +1,2 @@
+const text = "I Love" + " Bangladesh";
+console.log(text);
---
+++ string-methods.js
@@ -0,0 +1,16 @@
+// Extracting String Parts
+// There are 3 methods for extracting a part of a string:
+
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+
+
+// JavaScript String slice()
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(7, 13);
+
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(-12, -6);
+
+let part = str.slice(7);

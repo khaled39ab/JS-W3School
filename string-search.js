412500b42db6e6135ab5e545bbc8cb7123c6336a
khaled39ab <khaled39ab@gmail.com>
2022-01-25 21:48:03 +0600

    string-search

---
+++ string-search.js
@@ -0,0 +1,65 @@
+// JavaScript Search Methods
+// String indexOf()
+let str = "Please locate where 'locate' occurs!";
+let str2 = str.indexOf("locate");
+console.log(str2);
+{
+    let str = "Please locate where 'locate' occurs!";
+    str.indexOf("locate", 15);
+}
+
+// String lastIndexOf()
+let str3 = "Please locate where 'locate' occurs!";
+let str4 = str.lastIndexOf("locate");
+console.log(str4);
+
+{
+    let str = "Please locate where 'locate' occurs!";
+    str.lastIndexOf("locate", 15);
+}
+
+{
+    let str = "Please locate where 'locate' occurs!";
+    let str2 = str.lastIndexOf("John"); //return -1 for no match
+}
+
+// String startsWith()
+{
+    let text = "Hello world, welcome to the universe.";
+    let txt = text.startsWith("Hello"); //returns true if a string begins with a specified value, otherwise false
+
+}
+{
+    let text = "Hello world, welcome to the universe.";
+    text.startsWith("world")    // Returns false
+
+    let text1 = "Hello world, welcome to the universe.";  
+    text1.startsWith("world", 5)    // Returns false
+
+    let text2 = "Hello world, welcome to the universe.";   
+    text2.startsWith("world", 6)    // Returns true
+}
+
+// String endsWith()
+
+//String match()
+{
+    let text = "The rain in SPAIN stays mainly in the plain";
+    let txt = text.match(/ain/g); //returns the matches, as an Array object.
+    console.log(txt);
+}
+{
+    let text = "The rain in SPAIN stays mainly in the plain";
+    let txt = text.match(/ain/gi); //Perform a global, case-insensitive search for "ain"
+    console.log(txt);
+}
+
+// String includes()
+{
+    let text = "Hello world, welcome to the universe.";
+    let txt = text.includes("world"); //returns true if a string contains a specified value, otherwise false
+}
+{
+    let text = "Hello world, welcome to the universe.";
+    let txt = text.includes("world", 12); //starting the search at position 12 and return false
+}

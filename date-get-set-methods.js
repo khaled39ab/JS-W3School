c0e7a5a780f0cf708a123892d526a58b5c3df913
khaled39ab <khaled39ab@gmail.com>
2022-01-27 14:31:25 +0600

    date get & set methods

---
+++ date-get-set-methods.js
@@ -0,0 +1,58 @@
+//==================================== get Methods =================================
+// The getTime() Method
+// The getTime() method returns the number of milliseconds since January 1, 1970
+{
+    const d = new Date();
+    d.getTime();
+}
+
+// The getFullYear() Method
+{
+    const d = new Date();
+    d.getFullYear();
+}
+
+// The getMonth() Method
+{
+    const d = new Date();
+    d.getMonth();
+}
+
+// The getDate() Method
+{
+    const d = new Date();
+    d.getDate();
+}
+
+// The getHours() Method
+{
+    const d = new Date();
+    d.getHours();
+}
+
+// The getMinutes() Method
+{
+    const d = new Date();
+    d.getMinutes();
+}
+
+// The getSeconds() Method
+{
+    const d = new Date();
+    d.getSeconds();
+}
+
+// The getMilliseconds() Method
+{
+    const d = new Date();
+    d.getMilliseconds();
+}
+
+// The getDay() Method
+{
+    const d = new Date();   
+    console.log(d.getDay()); //In JavaScript, the first day of the week (0) means "Sunday"
+}
+
+
+

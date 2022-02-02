7cfc3d67749c41f23d2853cb02948cd9c4e60044
khaled39ab <khaled39ab@gmail.com>
2022-02-02 22:34:55 +0600

    Automatic Type Conversion

--- type-conversion.js
+++ type-conversion.js
@@ -52,3 +52,13 @@
     false.toString()   // returns "false"
     true.toString()    // returns "true"
 }
+
+// Automatic Type Conversion
+// When JavaScript tries to operate on a "wrong" data type, it will try to convert the value to a "right" type.
+{
+    5 + null    // returns 5         because null is converted to 0
+    "5" + null  // returns "5null"   because null is converted to "null"
+    "5" + 2     // returns "52"      because 2 is converted to "2"
+    "5" - 2     // returns 3         because "5" is converted to 5
+    "5" * "2"   // returns 10        because "5" and "2" are converted to 5 and 2
+}

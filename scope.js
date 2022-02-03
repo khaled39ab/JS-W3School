971ce6c5bb700017714b9c98304bcf5e1faf4000
khaled39ab <khaled39ab@gmail.com>
2022-02-03 23:53:00 +0600

    block scope

--- scope.js
+++ scope.js
@@ -3,3 +3,13 @@
 // Block scope
 // Function scope
 // Global scope
+
+
+// Block Scope
+// Variables declared inside a { } block cannot be accessed from outside the block
+{
+    {
+        let x = 2;
+    }
+    // x can NOT be used here
+}

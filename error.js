b318cc2642bd829826f2ad7bddc1a409c3b2402c
khaled39ab <khaled39ab@gmail.com>
2022-02-03 23:49:41 +0600

    uri error

--- error.js
+++ error.js
@@ -69,3 +69,14 @@
       console.log(err);
     }
 }
+
+// URI (Uniform Resource Identifier) Error
+// A URIError is thrown if you use illegal characters in a URI function
+{
+    try {
+        decodeURI("%%%");   // You cannot URI decode percent signs
+      }
+      catch(err) {
+        console.log(err);
+      }
+}

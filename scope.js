619ead2e385c920b574abcd5ee3b52868d2bf6b3
khaled39ab <khaled39ab@gmail.com>
2022-02-04 22:43:34 +0600

    function scope

--- scope.js
+++ scope.js
@@ -41,23 +41,5 @@ function myFunction() {
     }
 }
 
-// Global JavaScript Variables
-// A variable declared outside a function, becomes GLOBAL
-{
-    let carName = "Volvo";
-    // code here can use carName
-    
-    function myFunction() {
-    // code here can also use carName
-    }
-}
 
-// Global Scope
-// Variables declared Globally (outside any function) have Global Scope.
-// Global variables can be accessed from anywhere in a JavaScript program.
-// Variables declared with var, let and const are quite similar when declared outside a block
-{
-    var x = 2;       // Global scope
-    let y = 2;       // Global scope
-    const z = 2;       // Global scope
-}
+

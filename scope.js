93f6ef1b31c38f70499dd9b868e30a868a53d952
khaled39ab <khaled39ab@gmail.com>
2022-02-04 22:42:49 +0600

    function scope

--- scope.js
+++ scope.js
@@ -13,3 +13,51 @@
     }
     // x can NOT be used here
 }
+
+// Local Scope
+// Variables declared within a JavaScript function, become LOCAL to the function
+{
+// code here can NOT use carName
+
+function myFunction() {
+    let carName = "Volvo";
+    // code here CAN use carName
+  }
+  
+  // code here can NOT use carName
+}
+
+// Function Scope
+// JavaScript has function scope: Each function creates a new scope
+{
+    function myFunction1() {
+        var carName = "Volvo";   // Function Scope
+      }
+    function myFunction2() {
+        let carName = "Volvo";   // Function Scope
+    }
+    function myFunction3() {
+        const carName = "Volvo";   // Function Scope
+    }
+}
+
+// Global JavaScript Variables
+// A variable declared outside a function, becomes GLOBAL
+{
+    let carName = "Volvo";
+    // code here can use carName
+    
+    function myFunction() {
+    // code here can also use carName
+    }
+}
+
+// Global Scope
+// Variables declared Globally (outside any function) have Global Scope.
+// Global variables can be accessed from anywhere in a JavaScript program.
+// Variables declared with var, let and const are quite similar when declared outside a block
+{
+    var x = 2;       // Global scope
+    let y = 2;       // Global scope
+    const z = 2;       // Global scope
+}

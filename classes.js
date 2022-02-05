7b0a7c53b98362728093eb6f15f03e48c2ccc1bf
khaled39ab <khaled39ab@gmail.com>
2022-02-05 22:55:15 +0600

    send parameters to Class methods

--- classes.js
+++ classes.js
@@ -14,3 +14,25 @@ let myCar2 = new Car("Audi", 2019);
 
 console.log("My car is " + myCar1.age() + " years old.");
 console.log("My car is " + myCar2.age() + " years old.");
+
+
+
+//send parameters to Class methods
+{
+    class Car {
+        constructor(name, year) {
+          this.name = name;
+          this.year = year;
+        }
+        age(x) {
+          return x - this.year;
+        }
+      }
+      
+      let date = new Date();
+      let year = date.getFullYear();
+      
+      let myCar = new Car("Ford", 2014);
+      
+      console.log("My car is " + myCar.age(year) + " years old.");
+}

79c33070506fcb1e6df0ada3ab33dcb258f6a3df
khaled39ab <khaled39ab@gmail.com>
2022-02-05 22:42:01 +0600

    creating class

---
+++ classes.js
@@ -0,0 +1,16 @@
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }    
+}
+
+let myCar1 = new Car("Ford", 2014);
+let myCar2 = new Car("Audi", 2019);
+
+console.log("My car is " + myCar1.age() + " years old.");
+console.log("My car is " + myCar2.age() + " years old.");

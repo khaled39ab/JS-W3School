4c0faba2c63f8ee0ba3e0574f982276208a445a0
khaled39ab <khaled39ab@gmail.com>
2022-03-29 23:53:35 +0600

    function call

--- function-call-bind-apply.js
+++ function-call-bind-apply.js
@@ -1,3 +1,9 @@
-// Function call
+// JavaScript Function call()
 
 
+// Method Reuse
+// With the call() method, you can write a method that can be used on different objects.
+
+// All Functions are Methods
+// In JavaScript all functions are object methods.
+// If a function is not a method of a JavaScript object, it is a function of the global object

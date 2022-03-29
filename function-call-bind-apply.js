dbdf5ff137d6d8384e69c2e2a369beeca628bb41
khaled39ab <khaled39ab@gmail.com>
2022-03-29 23:59:25 +0600

    function call

--- function-call-bind-apply.js
+++ function-call-bind-apply.js
@@ -7,3 +7,30 @@
 // All Functions are Methods
 // In JavaScript all functions are object methods.
 // If a function is not a method of a JavaScript object, it is a function of the global object
+const person = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  // This will return "John Doe":
+  person.fullName();  
+
+/* In the example above, this refers to the person object.
+this.firstName means the firstName property of this.
+Same as:
+this.firstName means the firstName property of person. */
+
+
+/* What is this?
+In JavaScript, the this keyword refers to an object.
+Which object depends on how this is being invoked (used or called).
+The this keyword refers to different objects depending on how it is used:
+In an object method, this refers to the object.
+Alone, this refers to the global object.
+In a function, this refers to the global object.
+In a function, in strict mode, this is undefined.
+In an event, this refers to the element that received the event.
+Methods like call(), apply(), and bind() can refer this to any object. */
